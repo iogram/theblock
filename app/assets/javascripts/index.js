9ae9d172e527194f4816129b5c00d60d71f4adca
Ian Ogram <iogram10.io@gmail.com>
2016-02-07 20:58:28 +0000

    fixxed some issues

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -53,11 +53,13 @@ $(document).ready( function() {
                 // $(document).on('click','.navbar-collapse.in',function(e) {
                 //     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
                 //         $(this).collapse('hide');
-                    // }
-                    // });
+                //     }
+                //     });
                     $("body").click(function(event) {
                         $(".navbar-collapse").collapse('hide');
                     });
- });
+                    
+});
+
  
  
